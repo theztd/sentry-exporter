79569baccc76b0f7811c139879db86daa821f09b
Marek Sirovy <msirovy@gmail.com>
2022-02-10 21:38:39 +0100

    HELP in metrics

--- main.go
+++ main.go
@@ -37,7 +37,8 @@ func main() {
 		if err != nil {
 			log.Panicln(err)
 		}
-		f.WriteString(fmt.Sprintf("sentry_events_received_per_hour{\"project\"=\"%s\"} %d\n", p.Slug, stats[0][1]))
+		f.WriteString("# HELP hourly received amount of events grouped by project-slug. Metric is counter\n")
+		f.WriteString(fmt.Sprintf("sentry_received_events{\"project\"=\"%s\"} %d\n", p.Slug, stats[0][1]))
 
 		// issues, err := sentryProjectIssues(p.Organization.Slug, p.Slug)
 		// if err != nil {
